04c8eeabfd9d09b3641491d63cdbeef2e87a2376
AngelGAVargas <vargas.angel.ga@gmail.com>
2022-11-24 23:45:31 -0300

    practicando js

--- js/test.js
+++ js/test.js
@@ -1,11 +1,13 @@
 //test de javaScript
-alert("Hola mundo");
-let cosas = ['llaves','anteojos','linterna','reloj','celular'];
-let consulta = cosas[prompt("ingrese un numero entre 0 y 4")];
-if (cosas>=0 && cosas<5) {
-    alert(consulta);
-} else {
-    alert("ingrese un numero valido");
-    return;    
-}
+alert("Tish es un rancio?, vamos a verlo.");
+var mochila = ['Si','No','Puede ser','Seguramente','de nashi'];
 
+var numero = prompt("ingrese un numero entre el 0 y 4");
+
+
+
+if (numero >= 0 && numero <5) {
+    alert(mochila[numero]);
+} else {
+    alert("el numeor seleccionado no es valido");
+}
